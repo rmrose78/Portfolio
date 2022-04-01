27f0b98a6f33589bf491f2eb2f520dfa95e40bf6
rmrose78 <ryan@Ryans-Air.attlocal.net>
2022-04-01 18:50:50 -0400

    fixed nav and dropdown, fixed links, started dark mode

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+// TOGGLE MENU
+
+function showHideMenu() {
+  document.getElementById("menu-container").classList.toggle("show");
+}
+
+// cursor
+document.addEventListener("DOMContentLoaded", () => {
+  let mousePosX = 0,
+    mousePosY = 0,
+    mouseCircle = document.getElementById("mouse-circle");
+
+  document.onmousemove = (e) => {
+    mousePosX = e.pageX;
+    mousePosY = e.pageY;
+  };
+
+  let delay = 6,
+    revisedMousePosX = 0,
+    revisedMousePosY = 0;
+
+  function delayMouseFollow() {
+    requestAnimationFrame(delayMouseFollow);
+
+    revisedMousePosX += (mousePosX - revisedMousePosX) / delay;
+    revisedMousePosY += (mousePosY - revisedMousePosY) / delay;
+
+    mouseCircle.style.top = revisedMousePosY + "px";
+    mouseCircle.style.left = revisedMousePosX + "px";
+  }
+  delayMouseFollow();
+});
