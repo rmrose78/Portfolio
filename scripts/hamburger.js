c06fc9bc4d68ed0514f91c808dd7373ef30b93d7
rmrose78 <ryan@Ryans-Air.attlocal.net>
2022-04-21 15:35:13 -0400

    Performance and Accessibility bugs

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,9 +1,9 @@
 /*  Credit for hamburger animation to Jesse Couch at (https://codepen.io/designcouch/pen/ExvwPY)  */
 
-$(document).ready(function () {
-  $("#nav-icon4").click(function () {
-    $(this).toggleClass("open");
-  });
-});
+// $(document).ready(function () {
+//   $("#nav-icon4").click(function () {
+//     $(this).toggleClass("open");
+//   });
+// });
 
 // const hamburger = document.getElementById("nav-icon4");
