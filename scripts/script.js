d66c1713c70e2146c8c59e68f1ba16f82f011602
rmrose78 <ryan@Ryans-Air.attlocal.net>
2022-05-09 10:38:57 -0400

    Javascript fixes for side nav and removed JQuery for performance

--- scripts/script.js
+++ scripts/script.js
@@ -6,40 +6,29 @@ const contactLink = document.getElementById("contact-link");
 const navSideContainer = document.getElementById("nav-insideContainer");
 const logoName = document.getElementById("logo-name");
 const hamburger = document.getElementById("nav-icon4");
-
-// TOGGLE MENU
-
-// hamburger.addEventListener("click", () => {
-//   hamburger.classList.toggle("open");
-//   document.getElementById("menu-container").classList.toggle("show");
-//   document.getElementById("nav").classList.toggle("transparent");
-//   document.getElementById("logo-r").classList.toggle("logo-change");
-// });
-
-function menuToggle() {
-  document.getElementById("menu-container").classList.toggle("show");
-  document.getElementById("nav").classList.toggle("transparent");
-
-  // hamburger.classList.toggle("open");
-}
-
-menuLinks.forEach((link) => {
-  link.addEventListener("click", menuToggle);
-});
-
-hamburger.addEventListener("click", menuToggle);
-// hamburger.addEventListener("key: 13", menuToggle);
-homeLink.addEventListener("click", menuToggle);
-aboutLink.addEventListener("click", menuToggle);
-contactLink.addEventListener("click", menuToggle);
-
-// Top scroll navbar
 const navLinkHome = document.getElementById("nav--link-home");
 const navLinkAbout = document.getElementById("nav--link-about");
 const navLinkContact = document.getElementById("nav--link-contact");
 const navLinkLinkedin = document.getElementById("fa-linkedin-id");
 const navLinkGithub = document.getElementById("fa-github-id");
 
+// TOGGLE MENU
+const menuToggle = () => {
+  document.getElementById("menu-container").classList.toggle("show");
+  document.getElementById("nav").classList.toggle("transparent");
+  hamburger.classList.toggle("open");
+};
+
+hamburger.addEventListener("click", menuToggle);
+homeLink.addEventListener("click", menuToggle);
+aboutLink.addEventListener("click", menuToggle);
+contactLink.addEventListener("click", menuToggle);
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", menuToggle);
+});
+
+// Top of window side Nav hidden
 window.addEventListener("scroll", function () {
   let windowPosition = window.scrollY > 0;
 
@@ -60,19 +49,20 @@ window.addEventListener("scroll", function () {
   navLinkGithub.classList.toggle("nav--link-textColor-active", windowPosition);
 });
 
-navSideContainer.addEventListener("hover", function () {
-  let windowPosition = window.scrollY > 0;
+// Top of window side nave show on hover
+const navSideShowHover = () => {
+  if (window.scrollY == 0) {
+    console.log("i'm triggered");
+    navSideContainer.classList.toggle("nav-insideContainer-active");
+    logoName.classList.toggle("logo-hide");
+    navSideContainer.classList.toggle("navbar-contents-acitve");
+    navLinkHome.classList.toggle("nav--link-textColor-active");
+    navLinkAbout.classList.toggle("nav--link-textColor-active");
+    navLinkContact.classList.toggle("nav--link-textColor-active");
+    navLinkLinkedin.classList.toggle("nav--link-textColor-active");
+    navLinkGithub.classList.toggle("nav--link-textColor-active");
+  }
+};
 
-  navSideContainer.classList.toggle(
-    "nav-insideContainer-active",
-    windowPosition
-  );
-  logoName.classList.toggle("logo-hide", windowPosition);
-  navSideContainer.classList.toggle("navbar-contents-acitve");
-
-  navLinkHome.classList.toggle("nav--link-textColor-active");
-  navLinkAbout.classList.toggle("nav--link-textColor-active");
-  navLinkContact.classList.toggle("nav--link-textColor-active");
-  navLinkLinkedin.classList.toggle("nav--link-textColor-active");
-  navLinkGithub.classList.toggle("nav--link-textColor-active");
-});
+navSideContainer.addEventListener("mouseenter", navSideShowHover);
+navSideContainer.addEventListener("mouseleave", navSideShowHover);
